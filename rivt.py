e7bd7bf5c7c1315fad71309dd27d8155eae9ec3d
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-01-06 21:52:59 +0100

    Parse some arguments

--- rivt.py
+++ rivt.py
@@ -4,6 +4,7 @@ rivt :: Ludicrously simple image sewer
 """
 from __future__ import print_function
 
+import argparse
 import sys
 
 
@@ -14,7 +15,21 @@ __license__ = "GPLv3"
 
 
 def main(argv=sys.argv):
-    print("!")
+    args = parse_argv(argv)
+
+
+def parse_argv(argv):
+    parser = argparse.ArgumentParser(
+        description=__description__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False)
+
+    misc_group = parser.add_argument_group("Miscellaneous", "Other options")
+    misc_group.add_argument('--version', action='version', version=__version__)
+    misc_group.add_argument('-h', '--help', action='help',
+                             help="show this help message and exit")
+
+    return parser.parse_args(argv[1:])
 
 
 if __name__ == '__main__':

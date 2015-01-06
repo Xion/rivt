64823d68b9d407aec5d8118c89b7369a7f36c825
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-01-06 21:48:07 +0100

    Boilerplate for Python package

--- rivt.py
+++ rivt.py
@@ -7,6 +7,12 @@ from __future__ import print_function
 import sys
 
 
+__version__ = "0.0.1"
+__description__ = "Ludicrously simple image sewer"
+__author__ = "Karol Kuczmarski"
+__license__ = "GPLv3"
+
+
 def main(argv=sys.argv):
     print("!")
 

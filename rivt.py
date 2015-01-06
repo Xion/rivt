73f3a88dc3329b24fa716a4ad038ef3b3a2f5e9c
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-01-06 21:38:07 +0100

    Hello, world

---
+++ rivt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+rivt :: Ludicrously simple image sewer
+"""
+from __future__ import print_function
+
+import sys
+
+
+def main(argv=sys.argv):
+    print("!")
+
+
+if __name__ == '__main__':
+    sys.exit(main() or 0)

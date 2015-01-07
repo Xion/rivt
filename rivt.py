9e134cad03c15dc008343db1aa423ecc4c9f3d39
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-01-07 20:38:20 +0100

    Add -s/--show for showing result using PIL.Image.show

--- rivt.py
+++ rivt.py
@@ -19,8 +19,14 @@ __license__ = "GPLv3"
 def main(argv=sys.argv):
     args = parse_argv(argv)
 
-    images = list(map(Image.open, args.images))
-    sew(images, output=args.output)
+    source_images = list(map(Image.open, args.images))
+    result_image = sew(source_images)
+
+    if args.show:
+        result_image.show()
+    else:
+        # TODO(xion): decide on output format based on input formats
+        result_image.save(args.output, 'PNG')
 
 
 def parse_argv(argv):
@@ -32,11 +38,18 @@ def parse_argv(argv):
     parser.add_argument('images', type=str, nargs='+',
                         help="image files to sew together",
                         metavar="IMAGE")
-    parser.add_argument('-o', '--output',
-                        type=argparse.FileType('wb', 0), default='-',
-                        help="name of the output image file",
-                        metavar="OUTPUT")
-    # TODO(xion): add an option to show the image using PIL.Image.show
+
+    result_group = parser.add_argument_group(
+        "Result", "What to do with resulting image") \
+        .add_mutually_exclusive_group()
+    result_group.add_argument('-o', '--output',
+                              type=argparse.FileType('wb', 0), default='-',
+                              help="output the image to file with given name",
+                              metavar="OUTPUT")
+    result_group.add_argument('-s', '--show',
+                              action='store_true', default=False,
+                              help="show the resulting image "
+                                   "(in a system-specific way)")
 
     misc_group = parser.add_argument_group("Miscellaneous", "Other options")
     misc_group.add_argument('--version', action='version', version=__version__)
@@ -46,7 +59,7 @@ def parse_argv(argv):
     return parser.parse_args(argv[1:])
 
 
-def sew(images, output=sys.stdout):
+def sew(images):
     max_height = max(img.size[1] for img in images)
     adjusted_sizes = [
         (int(img.size[0] * (img.size[1] / float(max_height))), max_height)
@@ -61,8 +74,7 @@ def sew(images, output=sys.stdout):
         result.paste(image.resize(size, Image.BILINEAR), (x, 0))
         x += size[0]
 
-    # TODO(xion): decide on output format based on input formats
-    result.save(output, 'PNG')
+    return result
 
 
 if __name__ == '__main__':

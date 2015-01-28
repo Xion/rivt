e216b7dad5ab34b9e35960ff1d615d65e21634ed
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-01-28 13:01:50 -0800

    Detect when --show is impossible on non-OSX Unix systems

--- rivt/__main__.py
+++ rivt/__main__.py
@@ -2,6 +2,10 @@
 """
 Executable script.
 """
+from __future__ import print_function
+
+from distutils.spawn import find_executable
+import os
 import sys
 
 from PIL import Image
@@ -10,18 +14,40 @@ from rivt.args import parse_argv
 from rivt.logic import sew
 
 
+__all__ = ['main']
+
+
 def main(argv=sys.argv):
+    """Entry point."""
     args = parse_argv(argv)
 
     source_images = list(map(Image.open, args.images))
     result_image = sew(source_images, args.axis)
 
     if args.show:
-        result_image.show()
+        show_image(result_image)
     else:
         # TODO(xion): decide on output format based on input formats
         # and/or, more importantly, the extension of output file
         result_image.save(args.output, 'PNG')
+
+
+def show_image(image):
+    """Show given image to the user, if possible.
+    :param image: PIL :class:`Image` object
+    """
+    # check if PIL's Image.show() will actually do anything
+    non_osx_unix = os.name == 'posix' and sys.platform != 'darwin'
+    if non_osx_unix:
+        xv_exists = find_executable('xv') is not None
+        if not xv_exists:
+            # TODO(xion): devise an alternative; some possible options:
+            # * `display` from Imagemagick
+            # * `eog` on a temporary file
+            print("xv not found, can't show resulting image", file=sys.stdout)
+            return
+
+    image.show()
 
 
 if __name__ == '__main__':

992831cb1755ce0b4015777f1ccbf11a192f436c
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-01-29 16:56:06 -0800

    Use `display` from ImageMagick for --show if available on non-OSX Unixes

--- rivt/__main__.py
+++ rivt/__main__.py
@@ -8,7 +8,9 @@ from distutils.spawn import find_executable
 import os
 import sys
 
+import envoy
 from PIL import Image
+import six
 
 from rivt.args import parse_argv
 from rivt.logic import sew
@@ -25,7 +27,7 @@ def main(argv=sys.argv):
     result_image = sew(source_images, args.axis)
 
     if args.show:
-        show_image(result_image)
+        return show_image(result_image)
     else:
         # TODO(xion): decide on output format based on input formats
         # and/or, more importantly, the extension of output file
@@ -36,18 +38,23 @@ def show_image(image):
     """Show given image to the user, if possible.
     :param image: PIL :class:`Image` object
     """
-    # check if PIL's Image.show() will actually do anything
-    non_osx_unix = os.name == 'posix' and sys.platform != 'darwin'
-    if non_osx_unix:
-        xv_exists = find_executable('xv') is not None
-        if not xv_exists:
-            # TODO(xion): devise an alternative; some possible options:
-            # * `display` from Imagemagick
-            # * `eog` on a temporary file
-            print("xv not found, can't show resulting image", file=sys.stdout)
-            return
+    # use PIL's Image.show() only if it will actually do anything
+    is_posix = os.name == 'posix'
+    is_osx =  sys.platform == 'darwin'
+    if not is_posix or is_osx or find_executable('xv'):
+        return image.show()
 
-    image.show()
+    # use `display` binary from ImageMagick if available
+    if find_executable('display'):
+        image_data = six.StringIO()
+        image.save(image_data, 'PNG') ; image_data.seek(0)
+        return envoy.run('display -', data=image_data).status_code
+
+    # TODO(xion): devise more alternatives; some possible options:
+    # * `eog` on a temporary file
+    print("can't show resulting image without `xv` or `display` binaries",
+          file=sys.stdout)
+    return -1
 
 
 if __name__ == '__main__':
